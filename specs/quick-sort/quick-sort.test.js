623f1415b4c400bd6e878a364333d859823f4f1b
vinayaachar <43836914+vinayaachar@users.noreply.github.com>
2021-07-25 12:20:13 -0700

    quickSort

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -14,11 +14,29 @@
 
 function quickSort(nums) {
   // code goes here
+  //choose pivot point
+  if (nums.length < 2) {
+    return nums;
+  }
+
+  var pivot = nums.splice(Math.floor(Math.random() * nums.length - 1), 1)[0];
+
+  const left = [];
+  const right = [];
+
+  nums.forEach((item) => {
+    item < pivot ? left.push(item) : right.push(item);
+  });
+
+  const sortedLeft = quickSort(left);
+  const sortedRight = quickSort(right);
+
+  return sortedLeft.concat(pivot, sortedRight);
 }
 
 // unit tests
 // do not modify the below code
-test.skip("quickSort", function () {
+test("quickSort", function () {
   const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
   const answer = quickSort(input);
 
