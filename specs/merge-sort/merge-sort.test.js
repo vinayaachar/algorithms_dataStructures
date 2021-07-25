b07af7afa4fc9722760f1a05d2ac97efdcf64219
vinayaachar <43836914+vinayaachar@users.noreply.github.com>
2021-07-24 21:25:13 -0700

    merge_sort

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -8,11 +8,38 @@
 
 const mergeSort = (nums) => {
   // code goes here
+  // base case
+  if (nums.length < 2) {
+    return nums;
+  }
+
+  // splitting
+  let middle = Math.floor(nums.length / 2);
+  let left = nums.slice(0, middle);
+  let right = nums.slice(middle);
+
+  let leftSorted = mergeSort(left);
+  let rightSorted = mergeSort(right);
+
+  return merge(leftSorted, rightSorted);
+};
+
+const merge = (left, right) => {
+  let results = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+  return results.concat(left, right);
 };
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
