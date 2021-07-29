f225ac47b78566319eb1ab57b8e3c3fea8ec11c5
vinayaachar <43836914+vinayaachar@users.noreply.github.com>
2021-07-28 21:12:28 -0700

    Trees_LinkedList

--- specs/arrays/linkedlist.test.js
+++ specs/arrays/linkedlist.test.js
@@ -25,10 +25,76 @@
 
 class LinkedList {
   // code goes here
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const node = new Node(value);
+    this.length++;
+
+    if (!this.head) {
+      this.head = value;
+    } else {
+      // point tail to the node (pointer only)
+      this.tail.next = node;
+    }
+    // make actual tail the node
+    this.tail = node;
+  }
+
+  pop() {
+    return this.delete(this.length - 1);
+  }
+
+  _find(index) {
+    if (index >= this.length) return null;
+
+    let current = this.head;
+    for (let i = 0; i <= index - 1; i++) {
+      current = current.next;
+    }
+
+    return current;
+  }
+
+  get(index) {
+    const node = this._find(index);
+
+    if (!node) return null;
+    return node.value;
+  }
+
+  delete(index) {
+    if (index === 0) {
+      const head = this.head;
+      if (head) {
+        this.head = head.next;
+      } else {
+        this.head = null;
+        this.tail = null;
+      }
+      this.length--;
+      return head.value;
+    }
+
+    const node = this._find(index - 1);
+    const exc = node.next;
+    node.next = exc.next;
+    if (!node.next) this.tail = node.next;
+    this.length--;
+    return exc.value;
+  }
 }
 
 class Node {
   // code goes here
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 // unit tests
